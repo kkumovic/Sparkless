e5b12d58373d96d84e7e1645f6b889a5a5c01b78
kkumovic <kristijankumovic.kiki@gmail.com>
2023-07-28 14:34:50 +0200

    main menu
    trebalo bi da radi

--- Assets/Project/Scripts/LevelButton.cs
+++ Assets/Project/Scripts/LevelButton.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LevelButton : MonoBehaviour
+namespace Sparkless.Core
 {
-    // Start is called before the first frame update
-    void Start()
+    public class LevelButton : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Button _button;
+        [SerializeField] TMP_Text _levelText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private bool isLevelUnlocked;
+        private int currentLevel;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(Clicked);
+        }
+
+        private void OnEnable()
+        {
+            MainMenuManager.Instance.LevelOpened += LevelOpened;
+        }
+
+        private void OnDisable()
+        {
+            MainMenuManager.Instance.LevelOpened -= LevelOpened;
+        }
+
+        private void LevelOpened()
+        {
+            string gameObjectName = gameObject.name;
+            string[]parts = gameObjectName.Split('_');
+            _levelText.text = parts[parts.Length - 1];
+            currentLevel = int.Parse(_levelText.text);
+            isLevelUnlocked = GameManager.Instance.IsLevelUnlocked(currentLevel);
+        }
+
+        private void Clicked()
+        {
+            if (!isLevelUnlocked)
+                return;
+
+            GameManager.Instance.CurrentLevel = currentLevel;
+            GameManager.Instance.GoToGameplay();
+        }
+    } 
 }
